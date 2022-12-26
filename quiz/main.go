66067a6bb0f45b8b37f02df8f4a1fe7270c53b6d
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2022-12-26 15:54:50 +0300

    add solution to quiz part one

--- quiz/main.go
+++ quiz/main.go
@@ -2,43 +2,59 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"log"
 	"os"
+	"strings"
 )
 
-func startQuiz() error {
-	f, err := os.Open("problems.csv")
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
 	if err != nil {
-		log.Fatal(err)
-		return err
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
 	}
-	defer f.Close()
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
 
-	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal(err)
-		return err
+		exit(fmt.Sprintf("Failed to parse the provided CSV file."))
 	}
 
-	var answer string
-	totalQuestions := len(data)
-	totalScore := 0
+	problems := parseLines(lines)
 
-	for _, s := range data {
-		fmt.Printf("%s?", s[0])
-		fmt.Scanln(&answer)
-		if answer == s[1] {
-			totalScore += 1
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct += 1
 		}
 	}
-
-	fmt.Printf("You guessed %d/%d", totalScore, totalQuestions)
-
-	return nil
+	fmt.Printf("You scored %d out of %d.\n",correct, len(problems))
 }
 
-func main() {
-	startQuiz()
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit (msg string) {
+	fmt.Printf(msg)
+	os.Exit(1)
 }
