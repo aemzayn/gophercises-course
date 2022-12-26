532ec517b40121a38d85b1db2918004895e0be3c
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2022-12-26 16:09:40 +0300

    add quiz solution part two

--- quiz/main.go
+++ quiz/main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 
 	flag.Parse()
 
@@ -25,17 +27,30 @@ func main() {
 	}
 
 	problems := parseLines(lines)
-
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+
+problemloop:
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct += 1
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func ()  {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <- timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <- answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
-	fmt.Printf("You scored %d out of %d.\n",correct, len(problems))
+	fmt.Printf("\nYou scored %d out of %d.\n",correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
